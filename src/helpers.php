44e2f8d5de38c1efb3f678b9750feca792d9cb06
webloit-dev <webloit2020@gmail.com>
2023-09-01 04:00:29 +0100

    Misc Commit

--- src/helpers.php
+++ src/helpers.php
@@ -164,12 +164,12 @@ function rootDir()
 
 function error($key)
 {
-    session_start();
+    if(session_status() !== PHP_SESSION_ACTIVE) session_start();
 
     if(isset($_SESSION['zam_'.$key]))
     {
         return $_SESSION['zam_'.$key];
-        
+
         unset($_SESSION['zam_'.$key]);
     }
 }
